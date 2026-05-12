517e03b02f66bb43849e06a09e17e32ac5942d51
shivam-batham <shivambatham2610@gmail.com>
2026-05-12 11:02:15 +0530

    cookie fixed

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,14 +1,14 @@
 export function setAuthCookies(res, access_token, refresh_token){
     res.cookie("accessToken", access_token,{
         httpOnly:true,
-        sameSite:"lax",
+        sameSite:"strict",
         secure:true,
         // maxAge:process.env.ACCESS_TOKEN_EXPIRY
     })
 
     res.cookie("refreshToken", refresh_token, {
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "strict",
     secure: true,
     // maxAge: process.env.REFRESS_TOKEN_EXPIRY,
   });
